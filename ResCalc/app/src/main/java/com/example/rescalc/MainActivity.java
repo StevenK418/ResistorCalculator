89b16b69eac0143cb410be07fbc3f2b903e4562e
Steven Kelly <stevenkelly418@gmail.com>
2022-10-20 21:36:32 +0100

    => Updatedboth Java file and xml to contain base code for project going forward. =>This was for imrpoved simplicity before branching.

--- ResCalc/app/src/main/java/com/example/rescalc/MainActivity.java
+++ ResCalc/app/src/main/java/com/example/rescalc/MainActivity.java
@@ -7,46 +7,88 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.io.Console;
 import java.util.Dictionary;
 import java.util.Hashtable;
 
-public class MainActivity extends AppCompatActivity {
-    Dictionary firstDigits = new Hashtable();
-
-    String[] digitColourValues = getResources().getStringArray(R.array.colors);
-
-    Button firstDigitButton;
+public class MainActivity extends AppCompatActivity
+{
+    int currentBand = 0;
+    int resistorValue = 0;
+    TextView display;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Get a reference to the tens value selection button
-        firstDigitButton = findViewById(R.id.tensSelectorBtn);
-
-
+        display = findViewById(R.id.tvDisplay);
+    }
+    public void doBand1 (View view)
+    {
+        currentBand = 1;
+    }
+    public void doBand2 (View view)
+    {
+        currentBand = 2;
+    }
+    public void doBand3 (View view)
+    {
+        currentBand = 3;
     }
 
-
-
-    public void PopulateFirstDigits()
+    public void doRed (View view)
     {
-        for(int i = 0; i < digitColourValues.length; i++)
+        if (currentBand == 1)
         {
-            firstDigits.put(i, digitColourValues[i]);
+            resistorValue = 2;
+        }
+        else if (currentBand == 2)
+        {
+            resistorValue = resistorValue * 10 + 2;
+        }
+        else if (currentBand == 3)
+        {
+            resistorValue = resistorValue * 100;
         }
     }
 
-    public void tensBtnClick(View view)
+    public void doOrange(View view)
     {
-
-        //firstDigitButton.setBackgroundColor(digitColourValues[3]);
+        if (currentBand == 1)
+        {
+            resistorValue = 3;
+        }
+        else if (currentBand == 2)
+        {
+            resistorValue = resistorValue * 10 + 3;
+        }
+        else if (currentBand == 3)
+        {
+            resistorValue = resistorValue * 1000;
+        }
     }
 
+    public void doYellow (View view)
+    {
+        if (currentBand == 1)
+        {
+            resistorValue = 4;
+        }
+        else if (currentBand == 2)
+        {
+            resistorValue = resistorValue * 10 + 4;
+        }
+        else if (currentBand == 3)
+        {
+            resistorValue = resistorValue * 10000;
+        }
+    }
 
-
-
+    public void doCalculate (View view)
+    {
+        display.setText(String.valueOf(resistorValue));
+    }
 }
